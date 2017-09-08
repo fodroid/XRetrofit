a4468ffdb6a8d05545e9f51f44f6133f8bf34290
shihao <1406504841@qq.com>
2017-09-08 14:00:29 +0800

    初始化导入

---
+++ library/src/main/java/me/shihao/xretrofit/https/SSLParams.java
@@ -0,0 +1,13 @@
+package me.shihao.xretrofit.https;
+
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * Created by shihao on 2017/8/4.
+ */
+
+public class SSLParams {
+    public SSLSocketFactory sSLSocketFactory;
+    public X509TrustManager trustManager;
+}
